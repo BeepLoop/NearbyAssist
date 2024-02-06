300a811dc9030e1a864d8a4974d188fc17564347
John Loyd Mulit <jlmulit68@gmail.com>
2024-02-06 09:06:44 +0800

    feat: setup custom validator

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,17 +1,20 @@
 package server
 
 import (
+	"nearbyassist/internal/utils"
 	"net/http"
 
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.RemoveTrailingSlash())
+	e.Validator = &utils.Validator{Validator: validator.New()}
 
 	e.GET("/health", s.HealthHandler)
 
---
+++ internal/utils/validator.go
@@ -0,0 +1,15 @@
+package utils
+
+import "github.com/go-playground/validator"
+
+type Validator struct {
+	Validator *validator.Validate
+}
+
+func (v *Validator) Validate(i interface{}) error {
+	if err := v.Validator.Struct(i); err != nil {
+		return err
+	}
+
+	return nil
+}
