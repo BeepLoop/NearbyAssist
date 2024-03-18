85778c3f25e528de4409f2a4c727a40ff1c977cd
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-18 10:45:45 +0800

    chore: log dsn

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"nearbyassist/internal/types"
 	"os"
+
+	"github.com/labstack/gommon/log"
 )
 
 var Env *types.Config
@@ -11,6 +13,9 @@ func Init() error {
 	Env = &types.Config{
 		DSN: os.Getenv("DSN"),
 	}
+
+	dsn := os.Getenv("DSN")
+	log.Info("DSN: ", dsn)
 
 	return nil
 }
