0092950d3a6604e1538881710e25109877a71936
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-18 10:44:27 +0800

    refactor: removed godotenv

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/websocket v1.5.1
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.4
 )
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,18 +3,11 @@ package config
 import (
 	"nearbyassist/internal/types"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var Env *types.Config
 
 func Init() error {
-	_, err := godotenv.Read()
-	if err != nil {
-		return err
-	}
-
 	Env = &types.Config{
 		DSN: os.Getenv("DSN"),
 	}
