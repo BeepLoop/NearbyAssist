11afaabf4c2dca5431a48e884b2a3674b222a976
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-18 11:00:51 +0800

    chore: get rid of log

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,8 +3,6 @@ package config
 import (
 	"nearbyassist/internal/types"
 	"os"
-
-	"github.com/labstack/gommon/log"
 )
 
 var Env *types.Config
@@ -13,9 +11,6 @@ func Init() error {
 	Env = &types.Config{
 		DSN: os.Getenv("DSN"),
 	}
-
-	dsn := os.Getenv("DSN")
-	log.Info("DSN: ", dsn)
 
 	return nil
 }
