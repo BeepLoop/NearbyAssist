839645312a4c9c15ec23d3bafd32e632890db1ab
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-19 09:13:11 +0800

    refactor: use godotenv

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,24 @@ package config
 import (
 	"nearbyassist/internal/types"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var Env *types.Config
 
 func Init() error {
+	godotenv.Load()
+	var dsn string
+
+	if os.Getenv("GO_ENV") == "development" {
+		dsn = os.Getenv("DSN_DEV")
+	} else {
+		dsn = os.Getenv("DSN_PROD")
+	}
+
 	Env = &types.Config{
-		DSN: os.Getenv("DSN"),
+		DSN: dsn,
 	}
 
 	return nil
