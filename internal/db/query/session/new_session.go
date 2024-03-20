8d4b675f04ea6b4b576c2c68785d83087069e794
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-20 18:52:29 +0800

    feat: updates session table to logout past session when creating new session

--- internal/db/query/session/new_session.go
+++ internal/db/query/session/new_session.go
@@ -5,14 +5,31 @@ import (
 )
 
 func NewSession(username string, token string) error {
-	query := `
-        INSERT INTO
-            Session (userId, token)
-        VALUES
-            ((SELECT id FROM User WHERE name = ?), ?)
-    `
 
-	_, err := db.Connection.Exec(query, username, token)
+	tx, err := db.Connection.Beginx()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("UPDATE Session SET status = 'offline' WHERE userId = (SELECT id FROM User where name = ?) AND status = 'online'", username)
+	if err != nil {
+		if rollbackError := tx.Rollback(); rollbackError != nil {
+			return rollbackError
+		}
+
+		return err
+	}
+
+	_, err = tx.Exec("INSERT INTO Session (userId, token) VALUES ((SELECT id FROM User WHERE name = ?), ?)", username, token)
+	if err != nil {
+		if rollbackError := tx.Rollback(); rollbackError != nil {
+			return rollbackError
+		}
+
+		return err
+	}
+
+	err = tx.Commit()
 	if err != nil {
 		return err
 	}
