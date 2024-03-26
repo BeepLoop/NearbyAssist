f2c420864372339ad497650a5dbee71bd828d5d6
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-26 10:18:06 +0800

    fix: changed type of rate from float to string

--- internal/types/service.go
+++ internal/types/service.go
@@ -1,13 +1,13 @@
 package types
 
 type Service struct {
-	Id          int     `db:"id"`
-	Vendor      int     `query:"vendorId" db:"vendorId"`
-	Title       string  `query:"title" db:"title"`
-	Description string  `query:"description" db:"description"`
-	Rate        float64 `query:"rate" db:"rate"`
-	Location    string  `query:"location" db:"location"`
-	Category    string  `query:"category" db:"category"`
+	Id          int    `db:"id"`
+	Vendor      int    `query:"vendorId" db:"vendorId"`
+	Title       string `query:"title" db:"title"`
+	Description string `query:"description" db:"description"`
+	Rate        string `query:"rate" db:"rate"`
+	Location    string `query:"location" db:"location"`
+	Category    string `query:"category" db:"category"`
 }
 
 type TransformedServiceData struct {
@@ -15,7 +15,7 @@ type TransformedServiceData struct {
 	Vendor      int
 	Title       string
 	Description string
-	Rate        float64
+	Rate        string
 	Latitude    float64
 	Longitude   float64
 }
