c5a70a91c093ab614080530b7d7bf08f348baa88
John Loyd Mulit <jlmulit68@gmail.com>
2024-04-05 15:17:29 +0800

    feat: endpoint for approving application with given id

---
+++ internal/controller/service_vendor/v1/approve_application.go
@@ -0,0 +1,24 @@
+package service_vendor
+
+import (
+	vendor_query "nearbyassist/internal/db/query/service_vendor"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+func ApproveApplication(c echo.Context) error {
+	applicationId := c.Param("applicationId")
+	id, err := strconv.Atoi(applicationId)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "application ID must be a number")
+	}
+
+	err = vendor_query.ApproveApplication(id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, "Approve Application")
+}
--- internal/handlers/service_vendor/vendor_handler.go
+++ internal/handlers/service_vendor/vendor_handler.go
@@ -12,11 +12,15 @@ func VendorHandler(r *echo.Group) {
 	r.GET("/health", health.HealthCheck).Name = "vendor route health check"
 
 	r.GET("/count", service_vendor.CountVendors).Name = "get number of vendors"
+
 	r.GET("/vendor/:vendorId", service_vendor.GetVendor).Name = "get vendor details"
+
 	r.PATCH("/restrict/:vendorId", service_vendor.RestrictVendor).Name = "restrict vendor"
 	r.PATCH("/unrestrict/:vendorId", service_vendor.UnrestrictVendor).Name = "unrestrict vendor"
+
 	r.PUT("/application", service_vendor.VendorApplication).Name = "vendor application"
 	r.GET("/application", service_vendor.GetApplicants).Name = "get all vendor applications"
+	r.PATCH("/application/approve/:applicationId", service_vendor.ApproveApplication).Name = "approve vendor application"
 	r.PUT("/application/proof/upload", upload.VendorApplicationProof).Name = "upload vendor application proof"
 	r.GET("/application/count", service_vendor.CountPendingApplications).Name = "get number of vendor applications"
 }
