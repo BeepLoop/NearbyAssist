3c76bdd5851d945485fc3ba9308f59adf9f62e33
John Loyd Mulit <jlmulit68@gmail.com>
2024-04-06 18:01:08 +0800

    feat: implement method for category_model

--- internal/db/models/category_model.go
+++ internal/db/models/category_model.go
@@ -1,9 +1,19 @@
 package models
 
+import (
+	"context"
+	"nearbyassist/internal/db"
+	"time"
+)
+
 type CategoryModel struct {
 	Model
 	UpdateableModel
 	Title string `json:"title" db:"title"`
+}
+
+func NewCategoryModel() *CategoryModel {
+	return &CategoryModel{}
 }
 
 func (c *CategoryModel) Create() (int, error) {
@@ -16,4 +26,28 @@ func (c *CategoryModel) Update(id int) error {
 
 func (c *CategoryModel) Delete(id int) error {
 	return nil
+}
+
+func (c *CategoryModel) FindAll() ([]*CategoryModel, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	query := `
+        SELECT 
+            id, title
+        FROM 
+            Category
+    `
+
+	categories := make([]*CategoryModel, 0)
+	err := db.Connection.SelectContext(ctx, &categories, query)
+	if err != nil {
+		return nil, err
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return nil, context.DeadlineExceeded
+	}
+
+	return categories, nil
 }
