332411e74ba7d2403f0d5ab8602462d294efd56a
John Loyd Mulit <jlmulit68@gmail.com>
2024-04-19 19:05:01 +0800

    build: allow access to react frontend

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,12 +1,22 @@
 package server
 
-import "github.com/labstack/echo/v4/middleware"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func (s *Server) registerMiddleware() {
 	s.Echo.Pre(middleware.RemoveTrailingSlash())
+	s.Echo.Use(middleware.Recover())
+	s.Echo.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
+
+	s.Echo.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"127.0.0.1:5173", "localhost:5173"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+	}))
+
 	s.Echo.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status}\n",
 	}))
-	s.Echo.Use(middleware.Recover())
-	s.Echo.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
 }
