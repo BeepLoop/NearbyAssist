a4f420b7d7f703b5de5a18b27feb4d1bc7c7022d
John Loyd Mulit <jlmulit68@gmail.com>
2024-05-23 16:07:56 +0800

    fix: return adminId if user is admin/staff, else return userId

--- internal/utils/get_userId_from_jwt.go
+++ internal/utils/get_userId_from_jwt.go
@@ -13,6 +13,16 @@ func GetUserIdFromJWT(jwtSigner authenticator.Authenticator, authHeader string) 
 		return 0, err
 	}
 
+	// Check if jwt has role, if so, user is an admin or staff
+	if _, ok := claims["role"].(string); ok {
+		adminId, ok := claims["adminId"].(float64)
+		if !ok {
+			return 0, errors.New("Value adminId not found in claims")
+		}
+
+		return int(adminId), nil
+	}
+
 	userId, ok := claims["userId"].(float64)
 	if !ok {
 		return 0, errors.New("Value userId not found in claims")
