b4ed12144bda454d110f1ebf28b1a5a4f3b5de98
John Loyd Mulit <jlmulit68@gmail.com>
2024-09-10 13:25:33 +0800

    unfinished refactor: check token length

--- internal/middleware/check_role.go
+++ internal/middleware/check_role.go
@@ -10,7 +10,15 @@ import (
 
 func CheckRole(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		token := c.Request().Header.Get("Authorization")[len("Bearer "):]
+		token := c.Request().Header.Get("Authorization")
+		if token == "" {
+			return echo.NewHTTPError(http.StatusUnauthorized, models.Error{
+				Message: "Missing token",
+				Error:   "Missing token",
+			})
+		}
+
+		token = token[len("Bearer "):]
 		if err := auth.ValidateToken(token); err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, models.Error{
 				Message: "Invalid token",
