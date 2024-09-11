4b3301457bf809103c66324159cc2c1ec0856070
John Loyd Mulit <jlmulit68@gmail.com>
2024-09-12 01:53:46 +0800

    feat: implement db call

--- internal/store/application/mysql.go
+++ internal/store/application/mysql.go
@@ -1,7 +1,10 @@
 package application
 
 import (
+	"context"
 	"nearbyassist/internal/models"
+	"nearbyassist/internal/store"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -17,5 +20,28 @@ func NewMysqlApplicationStore(db *sqlx.DB) *MysqlApplicationStore {
 }
 
 func (s *MysqlApplicationStore) Create(data *models.ApplicationModel) (string, error) {
-	return "", nil
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if id, err := store.GenerateNanoId(); err != nil {
+		return "", err
+	} else {
+		data.Id = id
+	}
+
+	query := `
+        INSERT INTO
+            Application (id, applicantId, job, latitude, longitude)
+        VALUES
+            (:id, :applicantId, :job, :latitude, :longitude)
+    `
+	if _, err := s.db.NamedExecContext(ctx, query, data); err != nil {
+		return "", err
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return "", context.DeadlineExceeded
+	}
+
+	return data.Id, nil
 }
