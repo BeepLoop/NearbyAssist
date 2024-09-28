622b3c1601bff279267fe0ea9189318f8abec30a
John Loyd Mulit <jlmulit68@gmail.com>
2024-09-28 18:06:55 +0800

    fix: implement tag db functions

--- internal/store/tag/mysql.go
+++ internal/store/tag/mysql.go
@@ -1,7 +1,9 @@
 package tag
 
 import (
+	"context"
 	"nearbyassist/internal/models"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -21,9 +23,35 @@ func (s *MysqlTagStore) Create(data *models.TagModel) error {
 }
 
 func (s *MysqlTagStore) FindById(id string) (*models.TagModel, error) {
-	return nil, nil
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	tag := new(models.TagModel)
+	query := "SELECT id, title from Tag WHERE id = ?"
+	if err := s.db.GetContext(ctx, tag, query, id); err != nil {
+		return nil, err
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return nil, context.DeadlineExceeded
+	}
+
+	return tag, nil
 }
 
 func (s *MysqlTagStore) FindAll() ([]*models.TagModel, error) {
-	return nil, nil
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	tags := make([]*models.TagModel, 0)
+	query := "SELECT id, title from Tag"
+	if err := s.db.SelectContext(ctx, &tags, query); err != nil {
+		return nil, err
+	}
+
+	if ctx.Err() == context.DeadlineExceeded {
+		return nil, context.DeadlineExceeded
+	}
+
+	return tags, nil
 }
