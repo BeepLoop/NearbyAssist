0a66f3d832f8a48b41211a5e36f7b1282a0d94aa
John Loyd Mulit <jlmulit68@gmail.com>
2024-11-21 18:06:25 +0800

    feat: can search and display services in map

--- internal/handler/admin/map/map_handler.go
+++ internal/handler/admin/map/map_handler.go
@@ -1,11 +1,15 @@
 package map_handler
 
-import map_service "nearbyassist/internal/service/map"
+import (
+	map_service "nearbyassist/internal/service/map"
+	tag_service "nearbyassist/internal/service/tag"
+)
 
 type mapHandler struct {
+	tagService *tag_service.Service
 	mapService *map_service.Service
 }
 
-func NewHandler(mapService *map_service.Service) *mapHandler {
-	return &mapHandler{mapService: mapService}
+func NewHandler(mapService *map_service.Service, tagService *tag_service.Service) *mapHandler {
+	return &mapHandler{mapService: mapService, tagService: tagService}
 }
