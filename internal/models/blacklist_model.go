f12c1168ba096c5ebbff7a1df69098df5a6c17d1
John Loyd Mulit <jlmulit68@gmail.com>
2025-03-15 15:07:01 +0800

    chore: removed old commented code

--- internal/models/blacklist_model.go
+++ internal/models/blacklist_model.go
@@ -1,71 +1,7 @@
 package models
 
-// import (
-// 	"context"
-// 	"nearbyassist/internal/id_generator"
-// 	"time"
-//
-// 	"github.com/jmoiron/sqlx"
-// )
-
 type BlacklistModel struct {
 	Model
 	UpdateableModel
 	Token string `json:"token" db:"token"`
 }
-
-// func NewBlacklistModel(idGenerator id_generator.IdGenerator, conn *sqlx.DB) *BlacklistModel {
-// 	id, err := idGenerator.Generate()
-// 	if err != nil {
-// 		return nil
-// 	}
-//
-// 	return &BlacklistModel{
-// 		Model: Model{Id: id, Conn: conn},
-// 	}
-// }
-//
-// func NewBlacklistModelWithToken(token string, idGenerator id_generator.IdGenerator, conn *sqlx.DB) *BlacklistModel {
-// 	id, err := idGenerator.Generate()
-// 	if err != nil {
-// 		return nil
-// 	}
-//
-// 	return &BlacklistModel{
-// 		Model: Model{Id: id, Conn: conn},
-// 		Token: token,
-// 	}
-// }
-//
-// func (b *BlacklistModel) Create() (string, error) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-// 	defer cancel()
-//
-// 	query := "INSERT INTO Blacklist (id, token) VALUES (:id, :token)"
-// 	if _, err := b.Conn.NamedExecContext(ctx, query, b); err != nil {
-// 		return "", err
-// 	}
-//
-// 	if ctx.Err() == context.DeadlineExceeded {
-// 		return "", context.DeadlineExceeded
-// 	}
-//
-// 	return b.Id, nil
-// }
-//
-// func (b *BlacklistModel) FindByToken(token string) (*BlacklistModel, error) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-// 	defer cancel()
-//
-// 	query := "SELECT id, token FROM Blacklist WHERE token = ?"
-//
-// 	if err := b.Conn.GetContext(ctx, b, query, b.Token); err != nil {
-// 		return nil, err
-// 	}
-//
-// 	if ctx.Err() == context.DeadlineExceeded {
-// 		return nil, context.DeadlineExceeded
-// 	}
-//
-// 	return b, nil
-// }
--- internal/models/service_tag_model.go
+++ internal/models/service_tag_model.go
@@ -1,13 +1,5 @@
 package models
 
-// import (
-// 	"context"
-// 	"nearbyassist/internal/id_generator"
-// 	"time"
-//
-// 	"github.com/jmoiron/sqlx"
-// )
-
 type ServiceTagModel struct {
 	Model
 	UpdateableModel
@@ -17,51 +9,3 @@ type ServiceTagModel struct {
 	// Additional fields for joining tag title
 	Title string `json:"title" db:"title"`
 }
-
-// func NewServiceTagModel(idGenerator id_generator.IdGenerator, conn *sqlx.DB) *ServiceTagModel {
-// 	id, err := idGenerator.Generate()
-// 	if err != nil {
-// 		return nil
-// 	}
-//
-// 	return &ServiceTagModel{
-// 		Model: Model{Id: id, Conn: conn},
-// 	}
-// }
-//
-// func (s *ServiceTagModel) Create() (string, error) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-// 	defer cancel()
-//
-// 	insertTag := `
-//         INSERT INTO
-//             ServiceTag (id, serviceId, tagId)
-//         VALUES
-//             (:id, :serviceId, (SELECT id FROM Tag WHERE title = :title))
-//     `
-// 	if _, err := s.Conn.NamedExecContext(ctx, insertTag, s); err != nil {
-// 		return "", err
-// 	}
-//
-// 	if ctx.Err() == context.DeadlineExceeded {
-// 		return "", context.DeadlineExceeded
-// 	}
-//
-// 	return s.Id, nil
-// }
-//
-// func (s *ServiceTagModel) Delete() error {
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-// 	defer cancel()
-//
-// 	query := "DELETE FROM ServiceTag WHERE id = ?"
-// 	if _, err := s.Conn.ExecContext(ctx, query, s, s.Id); err != nil {
-// 		return err
-// 	}
-//
-// 	if ctx.Err() == context.DeadlineExceeded {
-// 		return context.DeadlineExceeded
-// 	}
-//
-// 	return nil
-// }
