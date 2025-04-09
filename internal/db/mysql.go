8e0e388412068de81aad975eeebf76eca87daaa7
John Loyd Mulit <jlmulit68@gmail.com>
2025-04-09 11:24:02 +0800

    fix: use sqlx open instead of connect and maintain single instance

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -1,34 +1,25 @@
 package db
 
 import (
-	"log"
-	"time"
-
 	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
-const (
-	MAX_RETRIES = 10
+var (
+	instance *sqlx.DB
 )
 
 func NewMysql(config mysql.Config) (*sqlx.DB, error) {
-	retries := 0
-
-	for {
-		conn, err := sqlx.Connect("mysql", config.FormatDSN())
-		if err != nil {
-			log.Printf("Error connecting to database: %v\n", err)
-			if retries >= MAX_RETRIES {
-				return nil, err
-			}
-
-			log.Printf("Retrying in 5 seconds...\n")
-			time.Sleep(5 * time.Second)
-			continue
-		}
-
-		return conn, nil
+	if instance != nil {
+		return instance, nil
 	}
+
+	conn, err := sqlx.Open("mysql", config.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	instance = conn
+
+	return instance, nil
 }
