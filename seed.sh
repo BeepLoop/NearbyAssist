db539f6f1bc4b4b74f027369befee4b42522006a
John Loyd Mulit <jlmulit68@gmail.com>
2025-05-08 06:55:55 +0800

    fix: password field in fulfill reset request

--- seed.sh
+++ seed.sh
@@ -8,3 +8,4 @@ make migrate-up
 
 echo "seeding test admin account..."
 na-cli create -u admin -p admin -m admin@email.com -r admin -e -k ~/repos/capstone/key.txt -c 'root:secret@/nearbyassist'
+na-cli create -u admin2 -p admin2 -m admin2@email.com -r admin -e -k ~/repos/capstone/key.txt -c 'root:secret@/nearbyassist'
