097a6a1bddfda7cd9888def21f84f7c33d6f0600
John Loyd Mulit <jlmulit68@gmail.com>
2025-05-26 21:02:57 +0800

    fix: error sending message due to invalid date format

--- internal/handler/api/message/message_handler.go
+++ internal/handler/api/message/message_handler.go
@@ -118,7 +118,8 @@ func (h *messageHandler) SendMessage(c echo.Context) error {
 
 	message := &models.MessageModel{
 		Model: models.Model{
-			Id: req.Id,
+			Id:        req.Id,
+			CreatedAt: req.CreatedAt,
 		},
 		Sender:   req.Sender,
 		Receiver: req.Receiver,
--- internal/request/new_message.go
+++ internal/request/new_message.go
@@ -1,8 +1,9 @@
 package request
 
 type NewMessagePayload struct {
-	Id       string `json:"id" validate:"required"`
-	Sender   string `json:"sender" validate:"required"`
-	Receiver string `json:"receiver" validate:"required"`
-	Content  string `json:"content" validate:"required"`
+	Id        string `json:"id" validate:"required"`
+	Sender    string `json:"sender" validate:"required"`
+	Receiver  string `json:"receiver" validate:"required"`
+	Content   string `json:"content" validate:"required"`
+	CreatedAt string `json:"createdAt" validate:"required"`
 }
